e5e6c8525ad11f865a36dd16edc5a1152d6957e9
zafer ozturk <ozturkza@itu.edu.tr>
2012-11-19 22:50:47 +0200

    message sınıfına ilişkin varlık sınıfı ve veritabanı işlemi yapan sınıf yazıldı. veritabanı işlemi yapan MessageDAO henüz tamamlanmadı.

--- 1/src/java/com/myapp/wicket/MessageDAO.java
+++ 1/src/java/com/myapp/wicket/MessageDAO.java
@@ -9,7 +9,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
