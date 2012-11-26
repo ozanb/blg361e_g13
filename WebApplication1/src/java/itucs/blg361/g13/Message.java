49e6c5b6bbb842044ae9a58a10d5b554b5c9d697
Bahadir Eyup OZAN <ozanb@itu.edu.tr>
2012-11-26 17:44:37 +0200

    Message tablosu oluşturuldu, Message.java(Entity Bean) oluşturuldu.

---
+++ WebApplication1/src/java/itucs/blg361/g13/Message.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itucs.blg361.g13;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ozan
+ */
+public class Message {
+    
+    private int id;
+    private String baslik;
+    private Date tarih;
+    private String icerik;
+    private Boolean okundu_mu;
+    private int kimden;
+    private int kime;
+
+    public Message(){
+    }
+    
+    public Message(int id, String baslik, Date tarih, String icerik, 
+            Boolean okundu_mu, int kimden, int kime){
+        
+        this.id = id;
+        this.baslik = baslik;
+        this.tarih = tarih;
+        this.icerik = icerik;
+        this.okundu_mu = okundu_mu;
+        this.kimden = kimden;
+        this.kime = kime;
+    
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the baslik
+     */
+    public String getBaslik() {
+        return baslik;
+    }
+
+    /**
+     * @param baslik the baslik to set
+     */
+    public void setBaslik(String baslik) {
+        this.baslik = baslik;
+    }
+
+    /**
+     * @return the tarih
+     */
+    public Date getTarih() {
+        return tarih;
+    }
+
+    /**
+     * @param tarih the tarih to set
+     */
+    public void setTarih(Date tarih) {
+        this.tarih = tarih;
+    }
+
+    /**
+     * @return the icerik
+     */
+    public String getIcerik() {
+        return icerik;
+    }
+
+    /**
+     * @param icerik the icerik to set
+     */
+    public void setIcerik(String icerik) {
+        this.icerik = icerik;
+    }
+
+    /**
+     * @return the okundu_mu
+     */
+    public Boolean getOkundu_mu() {
+        return okundu_mu;
+    }
+
+    /**
+     * @param okundu_mu the okundu_mu to set
+     */
+    public void setOkundu_mu(Boolean okundu_mu) {
+        this.okundu_mu = okundu_mu;
+    }
+
+    /**
+     * @return the kimden
+     */
+    public int getKimden() {
+        return kimden;
+    }
+
+    /**
+     * @param kimden the kimden to set
+     */
+    public void setKimden(int kimden) {
+        this.kimden = kimden;
+    }
+
+    /**
+     * @return the kime
+     */
+    public int getKime() {
+        return kime;
+    }
+
+    /**
+     * @param kime the kime to set
+     */
+    public void setKime(int kime) {
+        this.kime = kime;
+    }
+}
