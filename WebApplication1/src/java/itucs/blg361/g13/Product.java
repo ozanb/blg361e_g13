d5b2367a4a5460ab9b81e899155499f00df8d357
Bahadir Eyup OZAN <ozanb@itu.edu.tr>
2012-11-27 01:25:12 +0200

    Product ve Stock (Entity Bean)

---
+++ WebApplication1/src/java/itucs/blg361/g13/Product.java
@@ -0,0 +1,123 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itucs.blg361.g13;
+
+/**
+ *
+ * @author ozan
+ */
+public class Product {
+    
+    private int id;
+    private String productKind;
+    private int nOfProduct;
+    private int weight;
+    private int price;
+    private int productPerDay;
+    
+    /**
+     *
+     */
+    public Product(){
+    
+    }
+    
+    public Product(int id, String productKind, int nOfProduct, int weight,
+            int price, int productPerDay){
+    
+        this.id = id;
+        this.productKind = productKind;
+        this.nOfProduct = nOfProduct;
+        this.weight = weight;
+        this.price = price;
+        this.productPerDay = productPerDay;
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the productKind
+     */
+    public String getProductKind() {
+        return productKind;
+    }
+
+    /**
+     * @param productKind the productKind to set
+     */
+    public void setProductKind(String productKind) {
+        this.productKind = productKind;
+    }
+
+    /**
+     * @return the nOfProduct
+     */
+    public int getnOfProduct() {
+        return nOfProduct;
+    }
+
+    /**
+     * @param nOfProduct the nOfProduct to set
+     */
+    public void setnOfProduct(int nOfProduct) {
+        this.nOfProduct = nOfProduct;
+    }
+
+    /**
+     * @return the weight
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the productPerDay
+     */
+    public int getProductPerDay() {
+        return productPerDay;
+    }
+
+    /**
+     * @param productPerDay the productPerDay to set
+     */
+    public void setProductPerDay(int productPerDay) {
+        this.productPerDay = productPerDay;
+    }
+    
+}
---
+++ WebApplication1/src/java/itucs/blg361/g13/Stock.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itucs.blg361.g13;
+
+/**
+ *
+ * @author ozan
+ */
+public class Stock {
+    
+    private int id;
+    private String productName;
+    private int nOfStock;
+    private String storePlace;
+    
+    public Stock(){
+    }
+    
+    public Stock(int id, String productName, int nOfStock, String storePlace){
+    
+        this.id = id;
+        this.productName = productName;
+        this.nOfStock = nOfStock;
+        this.storePlace = storePlace;
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @param productName the productName to set
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    /**
+     * @return the nOfStock
+     */
+    public int getnOfStock() {
+        return nOfStock;
+    }
+
+    /**
+     * @param nOfStock the nOfStock to set
+     */
+    public void setnOfStock(int nOfStock) {
+        this.nOfStock = nOfStock;
+    }
+
+    /**
+     * @return the storePlace
+     */
+    public String getStorePlace() {
+        return storePlace;
+    }
+
+    /**
+     * @param storePlace the storePlace to set
+     */
+    public void setStorePlace(String storePlace) {
+        this.storePlace = storePlace;
+    }
+}
